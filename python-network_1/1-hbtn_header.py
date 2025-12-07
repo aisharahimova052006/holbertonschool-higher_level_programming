0653cd5988ee00a0f5713fa6023e7352d6f53317
Aysha Rahimli <aisharahimova052006@gmail.com>
2025-12-07 05:27:14 -0500

    changes

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,7 +3,5 @@ import urllib.request
 import sys
 
 with urllib.request.urlopen(sys.argv[1]) as r:
-    x = r.getheader("X-Request-Id")
-    if x is None:
-        x = r.headers.get("X-Request-Id") or r.headers.get("x-request-id") or r.info().get("X-Request-Id")
-    print(x)
+    x = r.info()
+    print(x.get("X-Request-ID")

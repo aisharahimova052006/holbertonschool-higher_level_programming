95765da27480e85293366dc5346bb0f7b5e6ceff
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-02-24 11:46:13 -0500

    py late tasks

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-i = 0
-j = 0
-string = ''
-for n in range(0, 99):
-    i = n // 10 
-    j = n % 10  
-    str = "{}{}".format(i, j)  
-    print(str, end=', ')  
-    print('99', end='\n')
+for n in range(100):
+    if n < 99:
+        print("{:02d}, ".format(n), end="")
+    else:
+        print("{:02d}".format(n))

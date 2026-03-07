de5686a850a8153beff46e04f03e743edf30d6f5
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-03-07 08:29:54 -0500

    python late tasks

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""in this module we define a function which attributes are returned"""
+
+
+def lookup(obj):
+
+    """in this function we use dir function to return attributes of object"""
+    return dir(obj)

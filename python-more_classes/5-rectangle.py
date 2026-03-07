c507a4eecdd7e81a4ec2e1797a20f29311f518a5
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-03-07 08:04:15 -0500

    python late tasks

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 """we define a rectangle class here"""
-
-
 class Rectangle:
     """a simple rectangle class"""
-
-
-
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-
     @property
     def width(self):
         return self.__width
-
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -23,11 +16,9 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-
     @property
     def height(self):
         return self.__height
-
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -36,16 +27,13 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-
     def area(self):
         return self.__width * self.__height
-
     def perimeter(self):
         if self.__width != 0 and self.__height != 0:
             return 2*(self.__width + self.__height)
         else:
             return 0
-
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ''
@@ -60,11 +48,9 @@ class Rectangle:
             else:
                 string += '\n'
         return string
-
     def __repr__(self):
         code = f"Rectangle({self.__width}, {self.__height})"
         eval(code)
         return code
-
     def __del__(self):
         print("Bye rectangle...")

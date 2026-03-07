bf7fe97c8b3e8727acb3b96d786b2faa659ea6e3
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-03-07 08:46:39 -0500

    python late tasks

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""the module which defines base geometry class"""
 
 
 class BaseGeometry:
 
+    """Basegeometry class"""
+
     def area(self):
-        raise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")>

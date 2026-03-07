3213d2baf137d264b5675fab7106a5ff1e29ccdf
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-03-07 08:40:21 -0500

    python late tasks

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,12 +3,8 @@
 
 
 class MyList(list):
-
     """Class that extends list and adds a method to print it sorted."""
 
     def print_sorted(self):
         """Prints the list in ascending order."""
         print(sorted(self))
-
-    def __str__(self):
-        return "[" + ", ".join(str(x) for x in self) + "]"

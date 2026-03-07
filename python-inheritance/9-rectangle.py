04b60836e508e7805cd8e61059351c6f262cdd04
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-03-07 09:02:43 -0500

    python late tasks

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""the module which defines rectangle class"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
+
+    def area(self):
+        return self.__height*self.__width

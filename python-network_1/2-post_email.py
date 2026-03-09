9816f62b3a9638ced4c8b21e86c627a8aefd3d29
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-03-08 23:36:59 -0400

    python

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+"""send email with POST"""
+import sys
 import urllib.request
 import urllib.parse
-import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
-
-    req = urllib.request.Request(url, data=data)
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode("utf-8")
-        print(body)
+    email = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(email).encode("ascii")
+    req = urllib.request.Request(url, data=data, headers={"cfclearance": "true"})
+    with urllib.request.urlopen(req) as r:
+        print(r.read().decode("utf-8"))

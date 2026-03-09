0cdff2f4cb7950e427d9efde2f95673f3045d16e
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-03-08 23:39:16 -0400

    python

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+"""get page or show error code"""
+import sys
 import urllib.request
 import urllib.error
-import sys
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
     try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode("utf-8")
-            print(body)
+        with urllib.request.urlopen(sys.argv[1]) as r:
+            print(r.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        print("Error code:", e.code)
